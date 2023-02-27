24e3c1073b6f73e5fbd2e2425c794b89c8da0519
SUJOY <s.u.j.o.y.b.e.r.a.g.a.m.e.r@gmail.com>
2023-02-27 23:19:34 +0530

    Update alive.py

--- DEADLYSPAM/plugins/alive.py
+++ DEADLYSPAM/plugins/alive.py
@@ -8,23 +8,23 @@ PIC = config.ALIVE_PIC
 if config.ALIVE_PIC:
     DEADLY_PIC = PIC
 else:
-    DEADLY_PIC = "https://telegra.ph/file/c6f99c0b68ff07439ed72.jpg"
+    DEADLY_PIC = "https://telegra.ph//file/e752a752e078ea53204aa.jpg"
 
 hl = config.CMD_HNDLR
 
 
-DEADLY = "✯ 𝗗𝗲𝗮𝗱𝗹𝘆 𝗦𝗽𝗮𝗺 𝗛𝗲𝗿𝗲 ✯\n\n"
+DEADLY = "✯ 🥵𝘽𝙃𝙊𝙎𝘿𝙄𝙒𝘼𝙇𝘼 𝙎𝙋𝘼𝙈 𝘽𝙊𝙏 𝙁𝙄𝙍𝙀🥵 ✯\n\n"
 DEADLY += f"═══════════════════\n"
 DEADLY += f"• **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `4.0.0`\n"
 DEADLY += f"• **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `{version.__version__}`\n"
-DEADLY += f"• **ᴅᴇᴀᴅʟʏʙᴏᴛ ᴠᴇʀsɪᴏɴ**  : `{deadlyversion}`\n"
+DEADLY += f"• **sᴘᴀʀᴋʏʙᴏᴛ ᴠᴇʀsᴏɴ**  : `{deadlyversion}`\n"
 DEADLY += f"═══════════════════\n\n"   
 
 
 @BOT0.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
 async def alive(event): 
   if event.sender_id in SUDOERS:
-     Blaze = [[Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/DEADLY_SPAMBOT"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/DEADLY_SPAM_BOT")], [Button.url("• ʀᴇᴘᴏ •", "https://github.com/Team-Deadly/DEADLY-SPAMBOT")]]
-     await BOT0.send_file(event.chat_id, DEADLY_PIC, caption=DEADLY, buttons=Blaze) 
+     Blaze = [[Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/FUCKER_KIDSS"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/CRACK_FIGHTER_ON_FIRE")], [Button.url("• ʀᴇᴘᴏ •", "https://te.legra.ph/file/db7c6b18567b5e81165ad.mp4")]]
+     await BOT0.send_file(event.chat_id, DEADLY_PIC, caption=SPARKY, buttons=Blaze) 
   else:
-      await event.reply("**ᴅᴇᴘʟᴏʏ ʏᴏᴜʀ ᴏᴡɴ ᴅᴇᴀᴅʟʏ-ꜱᴘᴀᴍʙᴏᴛ!**") 
+      await event.reply("**𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐋𝐀𝐔𝐍𝐃 𝐒𝐏𝐀𝐌𝐁𝐎𝐓!**") 
